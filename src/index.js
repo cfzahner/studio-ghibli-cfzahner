e81170aa189c1eca05f7a35f992887e02ab840b8
Courtney <cfzahner@gmail.com>
2020-05-21 15:00:07 -0500

    Set a value for this.value and update async function

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import { getStudioGhibliStuff } from './api'
 
 import * as components from './components'
+import fetch from 'fetch'
 
 // '#root' is the 'mounting point' for our app inside of './index.html'
 const root = document.querySelector('#root')
-
 const state = {
   Table: {
     // This will update based on 'components.Radio' - defaults to 'films
     activeStuff: 'films',
-    headers: {
-      // TODO: Check out the data at: https://ghibliapi.herokuapp.com/
-      // Pick 3 properties from each set and make those the 'headers'
+    data_keys: {
       films: ['title', 'director', 'release date'],
       people: ['name', 'gender', 'age'],
       locations: ['name', 'climate', 'terrain'],
       species: ['name', 'eye_colors', 'hair_colors'],
       vehicles: ['name', 'vehicle_class', 'length']
+    },
+    headers: {
+      films: ['Title', 'Director', 'Release Date'],
+      people: ['Name', 'Gender', 'Age'],
+      locations: ['Name', 'Climate', 'Terrain'],
+      species: ['Name', 'Eye Colors', 'Hair Colors'],
+      vehicles: ['Name', 'Vehicle Class', 'Length']
     },
     stuff: []
   }
@@ -24,19 +29,21 @@ const state = {
 
 // 'state' is DEFAULT VALUE for NAMED PARAMETER 'st'
 const render = (st = state) => {
-  console.info(st)
   // 'innerHTML' will be updated with the HTML 'return'ed from the FUNCTIONAL COMPONENTS.
   root.innerHTML = `
-    ${components.Radio()}
-    ${components.Table(st.Table)}
-  `
+      ${components.Radio()}
+      ${components.Table(st.Table)}
+      `
 
   document.querySelectorAll('input').forEach(input => {
-    input.addEventListener('change', async function () {
-      console.log(this.value)
-
-      // update Table.activeStuff with 'this value'
-      // make the correct api call
+    input.addEventListener('click', function () {
+      this.value = state.Table.activeStuff
+      const grabURL = async () => {
+        const response = await window.fetch('https://ghibliapi.herokuapp.com/films/')
+        return response.json()
+      }
+      grabURL()
+        .then(data => console.log(data))
     })
   })
 }
