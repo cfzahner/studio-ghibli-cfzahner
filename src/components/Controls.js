423f946c9c771c51ede4676f34e046f237733cad
Manav Misra <manavm@visionwebsoft.com>
2020-05-21 09:20:17 -0500

    :bug: Add missing VALUE attributes

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,25 +1,25 @@
 export default () => `<div>
-  <input type="radio" name="endpoint" id="films" checked /><label for="films"
+  <input type="radio" name="endpoint" id="films" value="films" checked /><label for="films"
     >Films
   </label>
 </div>
 <div>
-  <input type="radio" name="endpoint" id="people" /><label for="people"
+  <input type="radio" name="endpoint" id="people" value="people" /><label for="people"
     >People</label
   >
 </div>
 <div>
-  <input type="radio" name="endpoint" id="locations" /><label for="locations"
+  <input type="radio" name="endpoint" id="locations" value="locations" /><label for="locations"
     >locations</label
   >
 </div>
 <div>
-  <input type="radio" name="endpoint" id="species" /><label for="species"
+  <input type="radio" name="endpoint" id="species" value="species" /><label for="species"
     >Species</label
   >
 </div>
 <div>
-  <input type="radio" name="endpoint" id="vehicles" /><label for="vehicles"
+  <input type="radio" name="endpoint" id="vehicles" value="vehicles" /><label for="vehicles"
     >Vehicles</label
   >
 </div>
