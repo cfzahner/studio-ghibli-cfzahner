4463e8e037cb669cb7191b71bdc3c9da45d59f13
Courtney <cfzahner@gmail.com>
2020-05-21 16:26:16 -0500

    Add a try catch to throw error

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,10 @@
 // NAMED EXPORT
 export const getStudioGhibliStuff = async (stuff) => {
-  const resp = await window.fetch(`https://ghibliapi.herokuapp.com/${stuff}`)
-  return resp.json()
+  try {
+    const resp = await window.fetch(`https://ghibliapi.herokuapp.com/${stuff}`)
+
+    return resp.json()
+  } catch (error) {
+    throw Error(error)
+  }
 }
