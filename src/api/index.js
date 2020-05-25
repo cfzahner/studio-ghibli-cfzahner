82a29add34f7dbaf8a6d2ab29ebf6ce77938f36e
Courtney <cfzahner@gmail.com>
2020-05-25 17:27:21 -0500

    Add a fetch for data and include a catch for error

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,10 @@
-// NAMED EXPORT
-export const getStudioGhibliStuff = async (stuff) => {
-  try {
-    const resp = await window.fetch(`https://ghibliapi.herokuapp.com/${stuff}`)
+// fetch data and convert to json. Watch (catch) for an error, throw if there is an error.
 
-    return resp.json()
+export const getStudioGhibli = async (stuff) => {
+  try {
+    const response = await window.fetch('https://ghibliapi.herokuapp.com{stuff}')
+
+    return response.json()
   } catch (error) {
     throw Error(error)
   }
