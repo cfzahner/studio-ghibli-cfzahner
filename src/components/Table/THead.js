9572aba876148229424336243463f4b81c8da3e3
Courtney <cfzahner@gmail.com>
2020-05-25 18:34:34 -0500

    Add function to map and create heading tags

--- src/components/Table/THead.js
+++ src/components/Table/THead.js
@@ -1,13 +1,7 @@
+// create a function to map to headings to create a heading in a th tag. Join created headings.
+
 function createTableHeaders (headings) {
   return headings.map(heading => `
   <th>${heading}</th>
   `).join(' ')
 }
-
-export default (headers) => `
-  <thead class="header">
-    <tr>
-      ${createTableHeaders(headers)}
-    </tr>
-  </thead>
-`
