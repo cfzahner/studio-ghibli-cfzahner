25f6cb247819b72a9727f91ae04cdf55b6dd03a3
Courtney <cfzahner@gmail.com>
2020-05-25 18:47:25 -0500

    Add export default for props to access thead and tbody

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,24 +1,11 @@
-/**
- * Give names to DEFAULT EXPORTS (they are anonymous ) as we IMPORT
- * These are just FUNCTIONS,
- * not OBJECTS - no DESTRUCTURING
- */
 import THead from './THead'
 import TBody from './TBody'
-// import { Table } from '..'
 
-/**
- * Whenever this FUNCTIONAL COMPONENT is INVOKED,
- * it will INVOKE the FUNCTIONAL COMPONENTS that it needs.
- *
- * All of that HTML will be RETURNed and will end up inside '#root'
- */
-// TODO: Receive stat and use appropriate 'props'
+// export default to allow props to go into table and access thead and tbody by using dot notaion.
 export default (props) =>
- `
-  <table><class="table"/>
-    ${THead(props.headers[props.activeStuff])}
-    ${TBody(props.stuff, props.dataKeys[props.activeStuff])}
-  </table
 `
-// what do you want to receive in export default () and what will be sent out in Thead and Tbody ()
+<table><class="table"/>
+${THead(props.headers[props.activeStuff])}
+${TBody(props.stuff, props.dataKeys[props.activeStuff])}
+</table>
+`
