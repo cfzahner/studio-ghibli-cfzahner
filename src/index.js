71782635c0bcf6012fef061ecabdc452ed7f2bcd
Courtney <cfzahner@gmail.com>
2020-05-25 17:59:30 -0500

    Add query selector to check if radio box has been selected

--- src/index.js
+++ src/index.js
@@ -28,13 +28,17 @@ const render = (st = state) => {
   root.innerHTML = `
   ${components.Radio}
   ${components.Table(st.Table)}`
-}
-// go grab all the inputs and for each input add an event listener for a click, if there is a click heard start the function. With the function, go into state. and active stuff within table. Find the value of active stuff and render the data from the Api.
 
-document.querySelectorAll('input').forEach(input => {
-  input.addEventListener('click', function () {
-    state.Table.activeStuff = this.value
-    renderDataFromAPI()
-    render()
+  // go grab all the inputs and for each input add an event listener for a click, if there is a click heard start the function. With the function, go into state. and active stuff within table. Find the value of active stuff and render the data from the Api.
+
+  document.querySelectorAll('input').forEach(input => {
+    input.addEventListener('click', function () {
+      state.Table.activeStuff = this.value
+      renderDataFromAPI()
+      render()
+
+      // check to see if the activestuff radio has been clicked
+      document.querySelector(`${state.Table.activeStuff}`).checked = true
+    })
   })
-})
+}
