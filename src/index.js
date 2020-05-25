0566367f82c1be95084ab090585f637e62a78343
Courtney <cfzahner@gmail.com>
2020-05-25 17:36:47 -0500

    Add table data to state

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import { getStudioGhibli } from './api'
 import * as components from './components'
+
+const root = document.querySelector('#root')
+const state = {
+  Table: {
+    activeStuff: 'films',
+    dataKeys: {
+      films: ['title', 'director', 'release_date'],
+      people: ['name', 'gender', 'age'],
+      locations: ['name', 'climate', 'terrain'],
+      species: ['name', 'eye_colors', 'hair_colors'],
+      vehicles: ['name', 'class', 'length']
+    },
+    headers: {
+      films: ['Title', 'Director', 'Release Date'],
+      people: ['Name', 'Gender', 'Age'],
+      locations: ['Name', 'Climate', 'Terrain'],
+      species: ['Name', 'Eye Colors', 'Hair Colors'],
+      vehicles: ['Name', 'Class', 'Length']
+    },
+    stuff: []
+  }
+}
