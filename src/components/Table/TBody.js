92f4b2e1e1f14a2c8ad975590486c209fa183385
Courtney <cfzahner@gmail.com>
2020-05-25 18:27:12 -0500

    Add function to return keys from data table

--- src/components/Table/TBody.js
+++ src/components/Table/TBody.js
@@ -1,21 +1,18 @@
-// TODO: Get 'stuff' from STATE and get this table built
+// create a function for table body that passes the arguments dataSet and keys to map over data in tableData to return data and keys to then be joined.
+
 function createTableBody (dataSet, keys) {
   return dataSet.map(data => `
   <tr class="data">
   ${createTableData(data, keys)}
   </tr>
-  `
-  ).join('')
+  `)
+    .join('')
 }
 
-function createTableData (data, keys) {
+// create a function for tableData to map to access keys from data to then join.
+
+function createTableData () {
   return keys.map((key) => `
   <td>${data[key]}</td>
   `).join('')
 }
-
-export default (stuff, propKeys) => `
-  <tbody>
-    ${createTableBody(stuff, propKeys)}
-  </tbody>
-`
